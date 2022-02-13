efd0308ba2e484f227277f476a52ddf260ba530e
Scott-fo <scottfo@live.com>
2022-02-13 21:12:45 +0000

    added TheGraph integration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
-
+require('dotenv').config();
 module.exports = {
   solidity: "0.8.4",
   networks: {
     hardhat: {
       chainId: 1337
     },
-    // mumbai: {
-    //   url: "https://rpc-mumbai.matic.today",
-    //   accounts: [process.env.pk]
-    // },
+    mumbai: {
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [`${process.env.NEXT_PUBLIC_PK}`]
+    },
     // polygon: {
     //   url: "https://polygon-rpc.com/",
     //   accounts: [process.env.pk]
